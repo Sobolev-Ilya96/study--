4f7da82acc789393c0da6a0ff7d8375473a2f2ce
IzZoRI <onehunter52@mail.ru>
2022-11-02 19:58:20 +0300

    up ex009 IntroMethod

--- Example009_IntroMethod/Program.cs
+++ Example009_IntroMethod/Program.cs
@@ -12,14 +12,20 @@ int c1 = 39;
 int a2 = 12;
 int b2 = 23;
 int c2 = 33;
-int a3 = 13;
+int a3 = 131;
 int b3 = 23;
-int c3 = 330;
+int c3 = 33;
 
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-int max = Max(max1, max2, max3);
+//int max1 = Max(a1, b1, c1);
+//int max2 = Max(a2, b2, c2);
+//int max3 = Max(a3, b3, c3);
+//int max = Max(max1, max2, max3);
+
+int max = Max(
+    Max(a1, b1, c1),
+    Max(a2, b2, c2),
+    Max(a3, b3, c3));
+
 
 //int max = a1;
 //if (b1 > max) max = b1;
